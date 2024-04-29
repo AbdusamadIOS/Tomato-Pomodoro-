58e035e1de270804c8f09f05200b1a4772d5ff58
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-04-29 19:52:19 +0500

    Initial Commit

---
+++ Tomato (Pomodoro)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tomato (Pomodoro)
+//
+//  Created by Abdusamad Mamasoliyev on 29/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
